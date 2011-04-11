22b600146956c0a748ef9e3d7c4ca2151f95f02c
Robert Speicher <rspeicher@gmail.com>
2011-04-10 23:38:40 -0400

    Add a simple Gemfile fixture and a spec helper method to read it

---
+++ spec/fixtures/simplest.rb
@@ -0,0 +1,4 @@
+source :rubygems
+
+gem 'rails', '3.0.6'
+gem 'mysql'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
 $:.unshift File.expand_path("../../lib", File.dirname(__FILE__))
 
+module SpecHelpers
+  def gemfile(name)
+    File.expand_path("./fixtures/#{name}.rb", File.dirname(__FILE__))
+  end
+end
+
 RSpec.configure do |config|
   config.filter_run focus: true
   config.run_all_when_everything_filtered = true
