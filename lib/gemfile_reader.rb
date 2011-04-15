497bd5a605acaa067eba2feb0854cd2085542183
Robert Speicher <rspeicher@gmail.com>
2011-04-14 20:45:25 -0400

    Slightly restructure GemfileReader
    I was going to do some stuff with undefining objects I didn't want people
    to have access to, but I think the $SAFE level protects it well enough.

--- lib/gemfile_reader.rb
+++ lib/gemfile_reader.rb
@@ -6,83 +6,79 @@ class GemfileReader
   # Represents a single <tt>gem</tt> entry in a gemfile
   class Entry < Struct.new(:name, :version, :path, :git); end
 
-  class << self
-    # Read a specified Gemfile, evaluating its contents inside an instance of
-    # {GemfileReader} and returning an array of {Entry} instances.
-    #
-    # @param [String] filename
-    # @return [Array] Array of {Entry} instances
-    def evaluate(file_contents)
-      gemfile = new
+  # Read a specified Gemfile, evaluating its contents inside an instance of
+  # {GemfileReader} and returning an array of {Entry} instances.
+  #
+  # @param [String] filename
+  # @return [Array] Array of {Entry} instances
+  def self.evaluate(file_contents)
+    gemfile = Sandbox.new
 
-      file_contents = scrub(file_contents)
+    file_contents = scrub(file_contents)
 
-      # Even after scrubbing the file contents above, we still
-      # want to evaluate the file in an elevated safety environment
-      # to avoid people doing nasty things
-      lambda {
-        $SAFE = 3
-        gemfile.instance_eval(file_contents)
-      }.call
-
-      return gemfile.gems
+    # Even after scrubbing the file contents above, we still
+    # want to evaluate the file in an elevated safety environment
+    # to avoid people doing nasty things
+    isolated do
+      gemfile.instance_eval(file_contents)
     end
 
-    private
+    gemfile.gems
+  end
 
-    # Scrub Gemfile contents of some specific potentially nasty things
-    def scrub(contents)
-      contents.
-        split("\n").
-        # Select only "gem ..." lines, ignoring multiple commands on a single line
-        select { |l| l.strip =~ /^gem [^;\$]+$/ }.
+  def self.isolated(&block)
+    proc {
+      $SAFE = 3
 
-        # Prevents a SecurityError when accessing ENV
-        each { |l| l.gsub!(/ENV[^\s]+/, '""') }.
+      yield if block_given?
+    }.call
+  end
 
-        # Join it back together and untaint
-        join("\n").untaint
+  private
+
+  # Scrub Gemfile contents of some specific potentially nasty things
+  def self.scrub(contents)
+    contents.
+      split("\n").
+      # Select only "gem ..." lines, ignoring multiple commands on a single line
+      select { |l| l.strip =~ /^gem [^;\$]+$/ }.
+
+      # Prevents a SecurityError when accessing ENV
+      each { |l| l.gsub!(/ENV[^\s]+/, '""') }.
+
+      # Join it back together and untaint
+      join("\n").untaint
+  end
+
+  class Sandbox
+    # The only options to `gem` we actually care about
+    VALID_OPTIONS = [:path, :git].freeze
+
+    # @attr [Array]
+    attr_reader :gems
+
+    def initialize
+      @gems = []
     end
-  end
 
-  # The only options to `gem` we actually care about
-  VALID_OPTIONS = [:path, :git].freeze
+    protected
 
-  # @attr [Array]
-  attr_reader :gems
+    def gem(name, *options)
+      g = Entry.new
 
-  def initialize
-    @gems = []
-  end
+      g.name = name
 
-  protected
-
-  def method_missing(m, *args)
-    # Ignore everything that's not defined below
-  end
-
-  def group(*args, &block)
-    yield
-  end
-
-  alias_method :groups, :group
-  alias_method :platforms, :group
-
-  def gem(name, *options)
-    g = Entry.new
-
-    g.name = name
-
-    options.each do |opt|
-      if opt.is_a?(String)
-        g.version = opt
-      elsif opt.is_a?(Hash)
-        # Apply remaining options from a Hash
-        opt.select! { |k,v| VALID_OPTIONS.include? k }
-        opt.each_pair { |k,v| g[k] = v }
+      options.each do |opt|
+        if opt.is_a?(String)
+          g.version = opt
+        elsif opt.is_a?(Hash)
+          # Apply remaining options from a Hash
+          opt.select! { |k,v| VALID_OPTIONS.include? k }
+          opt.each_pair { |k,v| g[k] = v }
+        end
       end
-    end
 
-    @gems << g
+      @gems << g
+    end
   end
 end
