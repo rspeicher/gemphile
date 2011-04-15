2740a926cb01af0aebecc17e05908ab4616d9eb8
Robert Speicher <rspeicher@gmail.com>
2011-04-14 22:18:48 -0400

    Configure mongoid

--- gemphile.rb
+++ gemphile.rb
@@ -1,7 +1,17 @@
 require 'bundler/setup'
+
+require 'json'
+require 'mongoid'
 require 'sinatra/base'
 
 class Gemphile < Sinatra::Base
+  configure do
+    Mongoid.configure do |config|
+      config.master = Mongo::Connection.new.db("gemphile_#{ENV['RACK_ENV']}")
+      config.allow_dynamic_fields = false
+    end
+  end
+
   post '/push' do
   end
 end
