5011f416736c81de4ed9166051e5454fb528b4df
Robert Speicher <rspeicher@gmail.com>
2011-04-16 23:13:28 -0400

    Add more info to the index page

--- app/views/index.rb
+++ app/views/index.rb
@@ -3,5 +3,9 @@ module Views
     def gems
       GemCount.order_by([[:count, :desc], [:name, :asc]]).limit(100).to_ary
     end
+
+    def repos
+      Repository.limit(10).order_by(:updated_at, :desc).to_ary
+    end
   end
 end
