f14b3bd42fb2da770df41c8a14efe502f357f41b
Robert Speicher <rspeicher@gmail.com>
2011-04-17 01:53:57 -0400

    Update deploy for daemon

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,14 +21,40 @@ role :app, domain
 role :web, domain
 role :db,  domain, :primary => true
 
-# after "deploy:symlink", "deploy:update_crontab"
-# namespace :deploy do
-#   desc "Update the crontab file"
-#   task :update_crontab, :roles => :db do
-#     run "cd #{release_path} && bundle exec whenever --update-crontab #{application}"
-#   end
-# end
+# DelayedJob
+namespace :delayed_job do
+  def rack_env
+    fetch(:rails_env, false) ? "RACK_ENV=#{fetch(:rails_env)}" : ''
+  end
 
-after "deploy", "deploy:cleanup"
+  def args
+    fetch(:delayed_job_args, "")
+  end
+
+  def roles
+    fetch(:delayed_job_server_role, :app)
+  end
+
+  desc "Stop the delayed_job process"
+  task :stop, :roles => lambda { roles } do
+    run "cd #{current_path};#{rack_env} bin/gemphile_worker stop"
+  end
+
+  desc "Start the delayed_job process"
+  task :start, :roles => lambda { roles } do
+    run "cd #{current_path};#{rack_env} bin/gemphile_worker start #{args}"
+  end
+
+  desc "Restart the delayed_job process"
+  task :restart, :roles => lambda { roles } do
+    run "cd #{current_path};#{rack_env} bin/gemphile_worker restart #{args}"
+  end
+end
+
+after "deploy:stop",    "delayed_job:stop"
+after "deploy:start",   "delayed_job:start"
+after "deploy:restart", "delayed_job:restart"
+
+after "deploy",         "deploy:cleanup"
 
 require 'bundler/capistrano'
