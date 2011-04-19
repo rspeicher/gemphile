6846ea18bc0ef779c4c5437f42d1d4452f1fd014
Robert Speicher <rspeicher@gmail.com>
2011-04-19 00:44:13 -0400

    More layout progress

--- app/views/index.rb
+++ app/views/index.rb
@@ -1,8 +1,8 @@
 module Views
   class Index < Layout
     def gems
-      # TODO: Limit?
-      Repository.gem_counts.sort { |a, b| b[:count] <=> a[:count] }
+      # FIXME: This is going to become a really, really inefficient way to do this
+      Repository.gem_counts.sort { |a, b| b[:count] <=> a[:count] }[0...10]
     end
 
     def repos
