5715a3dbfe8581c920322e0c13ec5333c0d22aa4
Robert Speicher <rspeicher@gmail.com>
2011-04-18 20:03:32 -0400

    First refactor of GemfileJob, with specs
    This class is still pretty messy and its specs are pretty stub-heavy.

--- app/jobs/gemfile_job.rb
+++ app/jobs/gemfile_job.rb
@@ -1,59 +1,103 @@
+require 'logger'
+
 class GemfileJob < Struct.new(:id)
+  # Raised when a repository's Gemfile 404s
+  class GemfileError < IOError; end
+
+  # Raised when a repository's gemspec 404s
+  class GemspecError < IOError; end
+
+  def logger
+    unless @logger
+      @logger = Logger.new(STDOUT)
+      @logger.datetime_format = ''
+    end
+    @logger
+  end
+
+  def logger=(logger)
+    @logger = logger
+  end
+
   def perform
     repo = Repository.find(id)
-    puts "Updating #{repo.to_s}"
-    url  = repo.url + '/raw/HEAD/Gemfile'
+    logger.info "Updating #{repo.to_s}"
 
-    filename = "#{RACK_ROOT}/tmp/#{object_id}.gemfile"
-
-    # Make sure Gemfile exists
-    if remote_file_exists?(url)
-      puts "  Gemfile exists, downloading"
-
-      # Download it
-      body = remote_read(url)
-
-      if body =~ /^gemspec$/m
-        puts "  Gemfile references gemspec"
-
-        # Download the gemspec
-        # TODO: Any examples of using different repo and gem names?
-        url = repo.url + "/raw/HEAD/#{repo.name}.gemspec"
-        if remote_file_exists?(url)
-          puts "  #{repo.name}.gemspec exists, downloading"
-
-          # Gemspec overrides Gemfile, so let's get that instead
-          # TODO: Allow gems in the Gemfile AND in the gemspec?
-          body = remote_read(url)
-
-          # Remove the downloaded Gemfile then change the filename we pass to gemfile_reader
-          File.unlink(filename) if File.exists?(filename)
-          filename.gsub!(/\.gemfile$/, '.gemspec')
-
-          puts "  Reading gemspec"
-
-          # TODO: Do we want to store that this repo uses a gemspec to avoid this charade in the future?
-        else
-          "  ** Failed to download #{repo.name}.gemspec"
-        end
-      else
-        puts "  Reading Gemfile"
-      end
-
-      # Write the file and run it through gemfile_reader
-      File.open(filename, 'w') { |f| f.puts body }
-      gems = `#{RACK_ROOT}/vendor/gemfile_reader/bin/gemfile_reader #{filename}`.strip
+    begin
+      gems = process_gemfile(repo)
 
       # Pass the returned JSON string to populate_gems
-      puts "  Populating gems"
+      logger.info "  Populating gems"
       repo.populate_gems(gems)
+    rescue GemfileError, GemspecError => e
+      logger.error "  #{e.message}"
+    end
+  end
 
-      # Remove the local file
-      File.unlink(filename) if File.exists?(filename)
+  # Downloads and processes a <tt>Gemfile</tt> for the given repository
+  #
+  # @param [Repository] repo
+  # @return [String] JSON-ready string returned by {gemfile_reader}
+  # @raises [GemfileError] When remote Gemfile not found
+  def process_gemfile(repo)
+    remote = repo.url + "/raw/HEAD/Gemfile"
+    local  = "#{RACK_ROOT}/tmp/#{self.object_id}.gemfile"
+
+    if remote_file_exists?(remote)
+      logger.info "  Gemfile exists; downloading"
+      body = remote_read(remote)
+
+      if body =~ /^gemspec$/m
+        # TODO: Currently these are mutually exclusive. Do we want to support definitions in both?
+        # TODO: Add repo field saying it uses gemspec to avoid this charade in the future?
+        logger.info "  Gemfile references gemspec"
+        return process_gemspec(repo)
+      else
+        logger.info "  Processing Gemfile"
+        return write_and_process(local, body)
+      end
+    else
+      raise GemfileError, "#{remote} does not exist"
+    end
+  end
+
+  # Downloads and processes a <tt>gemspec</tt> file for the given repository
+  #
+  # @param [Repository] repo
+  # @return [String] JSON-ready string returned by {gemfile_reader}
+  # @raises [GemspecError] When remote gemspec file not found
+  def process_gemspec(repo)
+    remote = repo.url + "/raw/HEAD/#{repo.name}.gemspec"
+    local  = "#{RACK_ROOT}/tmp/#{self.object_id}.gemspec"
+
+    if remote_file_exists?(remote)
+      logger.info "  #{repo.name}.gemspec exists; downloading"
+      body = remote_read(remote)
+
+      logger.info "  Processing gemspec"
+      return write_and_process(local, body)
+    else
+      raise GemspecError, "#{remote} does not exist"
     end
   end
 
   protected
+
+  # Writes a remote file body to a local file, processes that file through
+  # {gemfile_reader} and then deletes the local file, returning the result of
+  # {gemfile_reader}
+  #
+  # @param [String] filename
+  # @param [String] body
+  # @return [String] {gemfile_reader} result
+  def write_and_process(filename, body)
+    # Write the file and run it through gemfile_reader
+    File.open(filename, 'w') { |f| f.puts body }
+    gems = `#{RACK_ROOT}/vendor/gemfile_reader/bin/gemfile_reader #{filename}`.strip
+    File.unlink(filename) if File.exists?(filename)
+
+    gems
+  end
 
   def remote_file_exists?(url)
     Curl::Easy.http_head(url).response_code == 200
--- spec/app/jobs/gemfile_job_spec.rb
+++ spec/app/jobs/gemfile_job_spec.rb
@@ -16,4 +16,96 @@ describe GemfileJob do
 
     Delayed::Job.count.should eql(3)
   end
+
+  it "should be queued from an existing repository with gemspec changes" do
+    pending
+  end
+
+  # Shared setup steps for #perform, #process_gemfile and #process_gemspec
+  shared_examples "job setup" do
+    let(:repo) { Factory(:repository, owner: 'tsigo', name: 'gemphile', url: 'https://github.com/tsigo/gemphile') }
+    let(:job)  { GemfileJob.new(repo.id) }
+    let(:log)  { StringIO.new }
+
+    before do
+      job.logger = Logger.new(log)
+      # Set default stubs so we don't actually make HTTP requests
+      job.stubs(:remote_file_exists?).returns(true)
+      job.stubs(:remote_read).returns("")
+    end
+  end
+
+  describe "#perform" do
+    include_examples "job setup"
+
+    it "finds the repository" do
+      Repository.expects(:find).with(repo.id).returns(repo)
+      job.stubs(:process_gemfile) # Speeds this up
+      job.perform
+    end
+
+    it "calls process_gemfile" do
+      job.expects(:process_gemfile).returns(true)
+      job.perform
+    end
+
+    it "calls Repository#populate_gems with the results" do
+      job.expects(:process_gemfile).returns('[]')
+      Repository.any_instance.expects(:populate_gems).with('[]').returns(true)
+      job.perform
+    end
+  end
+
+  describe "#process_gemfile" do
+    include_examples "job setup"
+
+    context "when Gemfile exists" do
+      context "with gemspec" do
+        before do
+          job.expects(:remote_read).returns("gemspec")
+        end
+
+        it "defers to process_gemspec" do
+          job.expects(:process_gemspec).with(repo)
+          job.process_gemfile(repo)
+        end
+      end
+
+      context "without gemspec" do
+        let(:local_file) { "#{RACK_ROOT}/tmp/#{job.object_id}.gemfile" }
+
+        it "runs gemfile_reader, returning its output" do
+          job.expects(:write_and_process).with(local_file, '').returns('reader_output')
+          job.process_gemfile(repo).should eql("reader_output")
+        end
+      end
+    end
+
+    context "when Gemfile not found" do
+      it "should raise GemfileError" do
+        job.expects(:remote_file_exists?).returns(false)
+        expect { job.process_gemfile(repo) }.to raise_error(GemfileJob::GemfileError, /does not exist/)
+      end
+    end
+  end
+
+  describe "#process_gemspec" do
+    include_examples "job setup"
+
+    context "when gemspec exists" do
+      let(:local_file) { "#{RACK_ROOT}/tmp/#{job.object_id}.gemspec" }
+
+      it "runs gemfile_reader, returning its output" do
+        job.expects(:write_and_process).with(local_file, '').returns('reader_output')
+        job.process_gemspec(repo).should eql("reader_output")
+      end
+    end
+
+    context "when gemspec not found" do
+      it "should raise GemspecError" do
+        job.expects(:remote_file_exists?).returns(false)
+        expect { job.process_gemspec(repo) }.to raise_error(GemfileJob::GemspecError, /does not exist/)
+      end
+    end
+  end
 end
