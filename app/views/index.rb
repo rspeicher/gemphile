745b70a080327bf68fe693244e0ad43c44fb9953
Robert Speicher <rspeicher@gmail.com>
2011-04-19 14:26:46 -0400

    Add some copy to the index

--- app/views/index.rb
+++ app/views/index.rb
@@ -3,9 +3,5 @@ module Views
     def gems
       GemCount.order_by([:value, Mongo::DESCENDING]).limit(10).all.to_a
     end
-
-    def repos
-      Repository.limit(10).order_by(:updated_at, :desc).to_ary
-    end
   end
 end
