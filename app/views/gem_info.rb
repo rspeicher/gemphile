67f6a78003dda45af6a06c101036413ed648799f
Robert Speicher <rspeicher@gmail.com>
2011-04-21 16:49:25 -0400

    Add descriptions to repositories

--- app/views/gem_info.rb
+++ app/views/gem_info.rb
@@ -5,7 +5,10 @@ module Views
     end
 
     def repos
-      Repository.where('gem_entries.name' => @gem).order_by([[:watchers, Mongo::DESCENDING], [:forks, Mongo::DESCENDING]]).limit(50).to_ary
+      Repository.where('gem_entries.name' => @gem).
+        order_by([[:watchers, Mongo::DESCENDING], [:forks, Mongo::DESCENDING]]).
+        limit(50).to_ary.
+        map { |r| r.description = r.description[0..100]; r }
     end
   end
 end
